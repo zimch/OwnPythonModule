90dd71873be459fa338615537c9210af8e49136d
Grigory Shanin <gregoryshanin.job@gmail.com>
2023-09-16 20:59:51 +0300

    Minor

--- my_module/__init__.py
+++ my_module/__init__.py
@@ -6,7 +6,7 @@ def find_args():
 
     parser = argparse.ArgumentParser(prog="Helper", description="Описание программы")
 
-    parser.add_argument("-nums", "--numbers", type=json.loads, help="Список чисел в формате [a, b]")
-    parser.add_argument("-op", "--operation", type=str, choices=['+', '-', '*', '/'], help="Операция над числами", default="+")
+    parser.add_argument("-nums", "--numbers", type=json.loads, help="Список чисел в формате [a, b]", required=True)
+    parser.add_argument("-op", "--operation", type=str, choices=['+', '-', '*', '/'], help="Операция над числами", default="+", required=False)
 
     return parser.parse_args()
