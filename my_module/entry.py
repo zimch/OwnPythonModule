5a0e452cbb65a91b49c6237985211c24ccc3a804
zimch <gregoryshanin.job@gmail.com>
2023-10-01 21:24:40 +0300

    add gui and tests

--- my_module/entry.py
+++ my_module/entry.py
@@ -1,27 +1,7 @@
-from my_module import find_args
-from my_module.math_module import MathHelper
+from .gui.application import Application
 
 def app():
-    helper = MathHelper()
-    args = find_args()
+    app = Application([])
     
-    if len(args.numbers) != 2: 
-        print("Только 2 числа")
-        return
-    
-    try:
-        match args.operation:
-            case '+':
-                print(helper.plus(args.numbers[0], args.numbers[1]))
-            case '-':
-                print(helper.minus(args.numbers[0], args.numbers[1]))
-            case '*':
-                print(helper.multiply(args.numbers[0], args.numbers[1]))
-            case '/':
-                print(helper.division(args.numbers[0], args.numbers[1]))
-            case _:
-                print("Нет такой операции")
-                
-    except:
-        print("Что-то не так :(")
+    quit(app.exec())
         
